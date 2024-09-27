2950515900b305a116e461ddc5130d4a0c1eb36c
Marilzon <maxmaril@hotmail.com>
2024-09-26 23:23:41 -0300

    simple data types

---
+++ topics/01-data_types.py
@@ -0,0 +1,15 @@
+# BASIC DATA TYPES PYTHON
+
+x = 10  # int
+y = 10.5  # float
+user_name = "marilzon"  # string
+is_active = True  # bool
+none_value = None  # NoneTYpe, like a null
+
+# ADVANCED DATA TYPES
+friuts = ["apple", "banana", "mellons"]  # list
+point = (3, 4)  # tuple
+hero_skills = {"Name": "Snails", "SQUAD": "#Vomitsquad"} # dict
+unique_values = {1, 2, 3, 5}  # set
+byte_data = b"bytes"  # bytes
+multiple_byte_data = bytearray(b"bytes")  # bytearray
